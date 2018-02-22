e8b09348eaad9cf9cc4e4e18e45dcd20449bb570
hsy <hsy@172.28.1.1>
2018-02-22 15:41:13 +0900

    merge test commit
    1번 로컬저장소 파일 변경

--- HelloVertex3/src/main/java/com/sample/MainVerticle.java
+++ HelloVertex3/src/main/java/com/sample/MainVerticle.java
@@ -15,6 +15,8 @@ public class MainVerticle extends AbstractVerticle {
 		// TODO Auto-generated method stub
 		super.start();
 		
+		// merge test 1111
+		
 		log.info("deploy start...");
 		
 		// config loading...
